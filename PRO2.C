37d623c001ce0a45d9ea432f884ddf82d1a057a1
Bitsbytes <bitsbytes@localhost.localdomain>
2018-12-30 18:55:21 +0530

    C-language Programs

---
+++ PRO2.C
@@ -0,0 +1,11 @@
+/*******************************
+ * BITSBYTES
+ ********************************/
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the number \n");
+	scanf("%d",&a);
+	printf("%d",a+10);
+}
---
+++ PRO4.C
@@ -0,0 +1,17 @@
+/*****************************
+ * BITSBYTES
+ ****************************/
+//#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b=10.23;
+	a=b;//Type Casting upper Range to lower Range
+	printf("%d drived from %f",a,b);
+	int c=8;
+	a=3;
+	b=c/a;
+	printf("\n%f\n",b);
+}
+
