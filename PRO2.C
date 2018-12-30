ef6b8866d4901c04b34c0d33e23e5f15326ee02b
Bitsbytes <bitsbytes@localhost.localdomain>
2018-12-30 19:12:30 +0530

    Fifth Program of C-Language series

---
+++ PRO2.C
@@ -0,0 +1,11 @@
+/*************************
+	BITSBYTES
+*************************/
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("ENTER THE NUMBER \n");
+	scanf("%d",&a);
+	printf("%d\n",a+10);
+}
---
+++ PRO5.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{ 
+	int a,b;
+	printf("Enter 2 numbers");
+	scanf("%d %d",&a,&b);
+	printf("\n %d+%d=%d\n",a,b,a+b);
+	printf("\n %d-%d=%d\n",a,b,a-b);      
+	printf("\n %d*%d=%d\n",a,b,a*b);       
+	printf("\n %d/%d=%d\n",a,b,a/b);       
+	printf("\n %d \ %d",a,b);
+printf("=%d\n",a%b);       
+
+}
