1e31bdd52aaa6f1529ea661afc08c2f79067fb28
Bitsbytes <bitsbytes@localhost.localdomain>
2018-12-30 22:46:41 +0530

    Eight Program of C-Language

---
+++ PRO8.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	/***********************
+		Assigment Operators
+	************************/
+	int a,b;
+	a=10;
+	b=2;
+	a+=b;//a=a+b;
+	int c=a-b;
+	printf("10+2=%d\n",a);
+	a-=b;//a=a-b;
+	printf("12-2=%d\n",a);
+	a*=b;//a=a*b;
+	printf("10*2=%d\n",a);
+	a/=b;
+	printf("20/2=%d\n",a);
+}
