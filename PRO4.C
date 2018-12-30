fea86ded9bd0610c756c82eb17405245419dce7a
Bitsbytes <bitsbytes@localhost.localdomain>
2018-12-30 19:37:52 +0530

    Fourth Program of C-Language

---
+++ PRO4.C
@@ -0,0 +1,20 @@
+/********************
+ * BITSBYTES
+ ********************/
+
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b=10.23;
+	a=b;//type casting upper_Range to Lower Range
+	printf("%d drived from %f",a,b);
+	int c=8;
+	a=3;
+	/************************
+	 * LOWER_RANGE to Upper_Range
+	 ***************************/
+	b=c/a;//its should be give as output  2.666666
+	printf("\n b=%f",b);
+}
+
