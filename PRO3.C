26ebce97b0ae32a8712b1012b825c9ccb0a815ca
Bitsbytes <bitsbytes@localhost.localdomain>
2018-12-30 19:31:53 +0530

    Third Program of C-Language

---
+++ PRO3.C
@@ -0,0 +1,25 @@
+/*****************************
+ * BYTESBYTES
+ ****************************/
+#include<stdio.h>
+int main()
+{
+		signed int a;
+		unsigned int ab;
+		a=327768;
+		ab=-891;
+		printf("a=%d\n",a);
+		printf("ab=%d\n",ab);
+		long int b=93247198721;
+		printf("b=%ld\n",b);
+		float c=18.9;
+		printf("c=%f",c);
+		double cf=1020.40;
+		printf("\n%lf",cf);
+		long double df=1020.4023;
+		printf("\n%Lf",df);
+		char ac='A';
+		printf("ac=%c",ac);
+		printf("(int)ac=%d",ac);
+}
+
