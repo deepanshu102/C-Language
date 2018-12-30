4cfd87b2506e8a9a7b386d6b68226084418dda9a
Bitsbytes <bitsbytes@localhost.localdomain>
2018-12-31 00:35:40 +0530

    Tenth program of C-Language

---
+++ PRO10.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i=9;
+	i++;//POST INCREMENT
+	printf("i++=%d\n",i);
+	printf("++i=%d\n",++i); //++i PRE INCREMENT
+
+	printf("--i=%d\n",--i);//Pre-Decrement
+	printf("i--=%d\n",i--);
+	printf("i=%d\n",i);
+}
+
